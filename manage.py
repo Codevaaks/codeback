7c64a9430baace4f81ce5f505147222a4c6e9ac2
Matthew <matthew@thewongcode.net>
2020-12-15 20:31:40 -0700

    Fixed folder and directory structure.

--- manage.py
+++ manage.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
+import dotenv
 
 def main():
+    dotenv.read_dotenv()
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codevaak.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
